5f4344634b03f4de6463fee32f8132f41254b95c
Alkheroo <95020781+Alkheroo@users.noreply.github.com>
2021-12-15 06:57:40 +0100

    Update client-ntp.ps1

--- client-ntp.ps1
+++ client-ntp.ps1
@@ -1,13 +1,15 @@
-
-# =======================================================
+#=======================================================
 #
 # Scripts install client VTP created by Ashraf Alkheroo
 #
 # =======================================================
-### Pour lancer le script: le nom du script plus l'addresse ip du serveur
+### Pour lancer le script: le nom du script plus l'addresse ip du serveur NTP principal 
+### plus le nom ou l'address IP dU serveur secondaire.
+### EXemple: .\client-ntp.ps1 10.0.1.10 www.apple.com
 
 # Variables
 $NTPServer = $args[0]
+$NTPfallback = $args[1]
 
 # Configure NTP and restart service
 
@@ -16,8 +18,9 @@ Push-Location
 Set-Location HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\DateTime\Servers
 Set-ItemProperty . 0 $NTPServer
 Set-ItemProperty . "(Default)" "0"
+Set-ItemProperty . 1 $NTPfallback
 Set-Location HKLM:\SYSTEM\CurrentControlSet\services\W32Time\Parameters
-Set-ItemProperty . NtpServer $NTPServer
+Set-ItemProperty . NtpServer $NTPServer, $NTPfallback
 Pop-Location
 Stop-Service w32time
 Start-Service w32time
